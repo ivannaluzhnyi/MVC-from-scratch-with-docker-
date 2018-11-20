084d1140325922b40dffea90d6afee680caee60c
Ivan Naluzhnyi <inaluzhnyi@b-flower.com>
2018-11-20 22:40:25 +0100

    mvc from scratch with docker

---
+++ www/controllers/UsersController.class.php
@@ -0,0 +1,29 @@
+<?php
+class UsersController{
+
+
+	public function defaultAction(){
+		echo "User default";
+	}
+
+
+	public function addAction(){
+	
+
+		$v = new View("addUser", "front");
+		
+	}
+
+	public function loginAction(){
+	
+		$v = new View("loginUser", "front");
+		
+	}
+
+	public function forgetPasswordAction(){
+
+		$v = new View("forgetPasswordUser", "front");
+		
+	}
+
+}
